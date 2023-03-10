e1aa9a13cde9613d17f43bbcf9ea3166f78e655a
Ankur Mahajan <mahajanankur.nith@gmail.com>
2023-03-10 19:48:46 +0530

    initial commit to lock package name on PyPi

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# configs-py
-Python JSON configurations manager and profiler.
+# conman-py
+This module offers a package to manage and profile JSON configurations in Python-based libraries and web applications. It provides an environment-specific configuration management system, making it simpler to handle multiple secure and critical configurations in different environments.
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+requires = []
+
+setup(
+    include_package_data=True,
+    name="conman",
+    version='0.0.1',
+    description='Package for managing and profiling JSON configurations in Python-based libraries and web applications.',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    author='Ankur Mahajan',
+    author_email='mahajanankur.nith@gmail.com',
+    url='https://github.com/mahajanankur/conman-py/blob/main/README.md',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=requires,
+    license="Apache License",
+    python_requires=">= 3",
+    project_urls={
+        'Documentation': 'https://github.com/mahajanankur/conman-py/blob/main/README.md',
+        'Source': 'https://github.com/mahajanankur/conman-py',
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
