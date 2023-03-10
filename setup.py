183f276ddfe43b80347a15acd813eb883f4e181d
Ankur Mahajan <mahajanankur.nith@gmail.com>
2023-03-10 19:57:42 +0530

    initial commit to lock package name on PyPi

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ setup(
     long_description=open('README.md').read(),
     author='Ankur Mahajan',
     author_email='mahajanankur.nith@gmail.com',
-    url='https://github.com/mahajanankur/conman-py/blob/main/README.md',
+    url='https://mahajanankur.github.io',
     packages=find_packages(exclude=['tests*']),
-    install_requires=requires,
+    # install_requires=requires,
     license="Apache License",
     python_requires=">= 3",
     project_urls={
