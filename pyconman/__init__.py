1a5479ab448d4081b5bd0e004a5e9f6f17edaa47
Ankur Mahajan <mahajanankur.nith@gmail.com>
2023-03-14 15:58:23 +0530

    pip upload error fix

--- pyconman/__init__.py
+++ pyconman/__init__.py
@@ -1,8 +1,8 @@
 """Python JSON configurations manager and profiler."""
 __author__ = "Ankur Mahajan"
 __copyright__ = "Copyright 2023"
-__version__ = "0.0.1"
+__version__ = "1.0.1"
 __maintainer__ = "Ankur Mahajan"
 __email__ = "mahajanankur.nith@gmail.com"
 
-from pyconman.loader import ConfigLoader
+from pyconman.config_loader import ConfigLoader
